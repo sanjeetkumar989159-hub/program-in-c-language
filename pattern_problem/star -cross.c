8922fd7e37f8e85585ae806d5eba059400fbe1a8
sanjeet kumar <sumitkumar989159@gmail.com>
2026-04-25 21:37:02 +0530

    Add files via upload

---
+++ pattern_problem/star -cross.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main(){
+ /* 
+     *        *
+       *    *   
+         *    
+       *    *  
+     *         *
+    
+
+    */
+int n;
+
+printf("enter number of lines n:");
+scanf("%d",&n);
+for(int i=1; i<=n; i++)
+{   for(int j=1;j<=n;j++)
+        if(j==i || i+j == n+1)
+            printf(" * ");
+        else
+            printf("   ");
+    printf("\n");
+}
+
+return 0;
+}
